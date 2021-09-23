e903245d639925807457602661fe06df25b0f773
x3mator <x3mator@gmail.com>
2021-09-23 18:09:34 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const upBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button')
+const sidebar = document.querySelector('.sidebar')
+const container = document.querySelector('.container')
+const mainSlide = document.querySelector('.main-slide')
+const slidesCount = mainSlide.querySelectorAll('div').length
+
+let activeSlideIndex = 0
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
+
+upBtn.addEventListener('click', () => {
+  changeSlide('up')
+})
+
+downBtn.addEventListener('click', () => {
+  changeSlide('down')
+})
+
+function changeSlide(direction) {
+
+  if (direction === 'up') {
+    activeSlideIndex++
+    if (activeSlideIndex === slidesCount) {
+      activeSlideIndex = 0
+    }
+  } else if (direction === 'down') {
+    activeSlideIndex--
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesCount - 1
+    }
+  }
+  const height = container.clientHeight
+
+  mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
+
+  sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
+}
+//прокручивание роликом мыши
+window.addEventListener('mousewheel', function(e){
+  // console.log('wheel event', e.deltaY)
+  if (e.deltaY > 0) {
+    changeSlide('up')
+  } else if (e.deltaY < 0)  changeSlide('down')
+  }
+)
+
+document.addEventListener('keydown', e => {
+  if (e.key === 'ArrowUp') {
+    changeSlide('up')
+  } else if (e.key === 'ArrowDown') {
+    changeSlide('down')
+  }
+})
